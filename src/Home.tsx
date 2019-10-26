19ecbf05b746e63d737bdbf4dadd6ae5e2aa6a62
davjung <dav@u.northwestern.edu>
2019-10-26 18:11:13 -0400

    x

---
+++ src/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { any } from 'prop-types';
+
+
+const Home: React.FC<any> = () => {
+    return (
+        <View>
+
+                
+
+        </View>
+        )
+    }
+    
+export default Home;
+
